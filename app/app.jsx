aa800f88af6f8a9184234ea0399a1df86352d71d
Motoki Naito <tonponpon4s@gmail.com>
2016-11-29 19:43:25 -0800

    create TodoApp.jsx file

--- app/app.jsx
+++ app/app.jsx
@@ -2,13 +2,13 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory } = require('react-router');
 
-
+var TodoApp = require('TodoApp');
 // Load foundations
 $(document).foundation();
 
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <p>Boilerplate</p>,
+  <TodoApp />,
   document.getElementById('app')
 );
