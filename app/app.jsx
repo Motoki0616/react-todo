1d5906b5143edd4af84454953fed830faf28cee0
Motoki Naito <tonponpon4s@gmail.com>
2016-12-09 17:27:11 -0800

    binding with redux

--- app/app.jsx
+++ app/app.jsx
@@ -1,14 +1,28 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Provider} = require('react-redux');
 var {Route, Router, IndexRoute, hashHistory } = require('react-router');
 
 var TodoApp = require('TodoApp');
+
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+    console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('CleanThe yard'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted() );
 // Load foundations
 $(document).foundation();
 
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <TodoApp />,
+  <Provider store={store}>
+      <TodoApp/>
+  </Provider>,
   document.getElementById('app')
 );
