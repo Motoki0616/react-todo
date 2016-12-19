26dcd010d64daa9d381496487f5d3cffb493d940
Motoki Naito <tonponpon4s@gmail.com>
2016-12-18 17:39:29 -0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 module.exports = {
   entry: [
       'script!jquery/dist/jquery.min.js',
-      'script!foundation-sites/dist/foundation.min.js',
-      './app/app.jsx'
+        'script!foundation-sites/dist/foundation.min.js',
+        './app/app.jsx'
   ],
   externals: {
       jquery: 'jQuery'
