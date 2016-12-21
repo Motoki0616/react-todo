3fbdec587b9f0a0b6ae809639dcb9978e905d0e6
Motoki Naito <tonponpon4s@gmail.com>
2016-12-20 16:32:10 -0800

    Move firebase creds to en varibales

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 try {
     var config = {
-        apiKey: "AIzaSyBdESljHQXMD98zhMQ0v0w3OpU8FG32N7g",
-        authDomain: "motoki-todo-app.firebaseapp.com",
-        databaseURL: "https://motoki-todo-app.firebaseio.com",
-        storageBucket: "motoki-todo-app.appspot.com",
-        messagingSenderId: "229657876962"
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID
       };
 
     firebase.initializeApp(config);
