c1d5a0ed26f6d4f8cc5d3339b2d91faa5bdb5a00
Motoki Naito <tonponpon4s@gmail.com>
2016-12-21 17:40:27 -0800

    fixed search filter

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -1,6 +1,7 @@
 module.exports = {
     filterTodos: function(todos, showCompleted, searchText){
         var filteredTodos = todos;
+        searchText = searchText.toLowerCase();
 
         filteredTodos = filteredTodos.filter((todo)=>{
             return !todo.completed || showCompleted;
--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -9,7 +9,7 @@ export var TodoSearch = React.createClass({
             <div className="container__header">
                 <div>
                     <input type="search" ref="searchText" placeholder="Search todos" value={searchText} onChange={()=>{
-                            var searchText = this.refs.searchText.value.toLowerCase();
+                            var searchText = this.refs.searchText.value;
                             dispatch(actions.setSearchText(searchText));
                         }}/>
                 </div>
